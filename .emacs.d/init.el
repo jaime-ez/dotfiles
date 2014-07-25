8b3ecee26aaa2980858cd6857983baf82253581d
jaime-e <jechegarayz@gmail.com>
2014-07-25 19:38:18 +0000

    agregar newline para crontab

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -28,6 +28,9 @@
 (setq suggest-key-bindings t)
 (setq vc-follow-symlinks t)
 
+;;Agregar newline para crontab
+(setq require-final-newline t)
+
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
